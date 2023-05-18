c48b4f73b72eb7cbc90190a16c863b2cade44cda
Afuh Christian <afuhchristian10@gmail.com>
2023-05-18 08:12:38 +0100

    Added a documentation folder with and intro.md file for small documentation of what has been done so far

---
+++ Documentation/intro.md
@@ -0,0 +1,37 @@
+# 1 ------- main.dart 
+- The main container running the app 
+- All other wigets are being passed as a child to it
+- 
+
+# 2 ------- Layout_Builder.dart 
+- This is where the config for responsiveness is done .. 
+- It returns a mobile or desktop or table depending on the device width 
+
+# NB ----- Global_Components 
+- Contains sections of reusable code and can recieve parameters ...... 
+
+# 3 ------- We choose  one device to  work with for now .... -------------------------------------------------------
+
+# Screen_Sizes/MobileApp
+
+# i ---- MobileA_main.dart 
+- The wrapper for  the Mobile Components ..... 
+
+
+# ii ----- Pages/ ... 
+- Containers the different pages you can navigate to 
+- The HomePage is the default page here ..
+
+# ii ----- Components/ .... 
+- Contains small sections of custom reusable widgets 
+- The can recieve parameters 
+
+# iii ---- Constants 
+- Also contians small sections of reusable widgets 
+- Can't recieve paramters ... 
+
+
+
+
+
+
--- lib/Screen_Sizes/MobileApp/Components/Navbar.dart
+++ lib/Screen_Sizes/MobileApp/Components/Navbar.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
+import '../../Global_Components/Nav_Components/NavTitle.dart';
 import '../../Global_Components/Nav_Components/Nav_Drawer.dart';
+import '../../Global_Components/Nav_Components/Nav_avatar.dart';
 import '../Constants/Nav_Constants.dart';
 
 List navitems = [
@@ -36,7 +38,24 @@ class NavBarMobile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: appBar,
+        appBar: AppBar(
+    title: const AppTitleWiget(
+        width: 120, bookwidth: 22, bookheight: 22, titlefont: 21),
+    // title: Title(120 , 22 , 22 , 21),
+    backgroundColor: Colors.white,
+    centerTitle: true,
+    iconTheme: const IconThemeData(color: Colors.black),
+    actions: const [
+      UserFace(
+        image: "images/nouser.png",
+        imageHeight: 22,
+        imagewidth: 35,
+        top: 10,
+        bottom: 10,
+        right: 20,
+        raduis: 3000.0,
+      )
+    ]),
         drawer: const NavDrawer(
           top: 10,
           bottom: 10,
